Fixed scaling issue in dot product attention

growing/scaled_dot_product_attention.py
@@ -38,19 +38,23 @@ class ScaledDotProductAttention(GrowingModule):
 
         # Batch (b) and head (h) dimensions remain intact
         # Query dimension (q) remains in the same place
-        einsum_str = 'qbhi,kbhi->bhqk'
+        # Embedding values (e) are used in actual dot product
+        einsum_str = 'qbhe,kbhe->bhqk'
         product = torch.einsum(einsum_str, q, k)
 
         if self.new_neurons is not None:
-            q_novel = torch.nn.functional.linear(x, self._weight_dir[0], self._bias_dir[0]).view(length, batch_size, self.heads, -1)
-
+            q_novel = torch.nn.functional.linear(x, self._weight_dir[0], self._bias_dir[0])
+            q_novel = q_novel.view(length, batch_size, self.heads, -1)
             q_novel = q_novel * self.new_neurons
 
-            k_novel = torch.nn.functional.linear(x, self._weight_dir[1], self._bias_dir[1]).view(length, batch_size, self.heads, -1)
+            k_novel = torch.nn.functional.linear(x, self._weight_dir[1], self._bias_dir[1])
+            k_novel = k_novel.view(length, batch_size, self.heads, -1)
 
             product = product + torch.einsum(einsum_str, q_novel, k_novel)
 
-        return torch.softmax(product  / torch.sqrt(torch.tensor(self.d_head)), axis=-1)
+        product = product / torch.sqrt(torch.tensor(self.d_head))
+
+        return torch.softmax(product, axis=-1)
 
     def grow(self,
              num_novel : int = 0,
@@ -105,8 +109,8 @@ class ScaledDotProductAttention(GrowingModule):
 
             # copy old neurons
 
-            q_weight[:, :self.d_head] = self.q_linear.weight.view(self.heads, self.d_head, -1)
-            k_weight[:, :self.d_head] = self.k_linear.weight.view(self.heads, self.d_head, -1)
+            q_weight[:, :self.d_head] = self.q_linear.weight.view(self.heads, self.d_head, self.d_model)
+            k_weight[:, :self.d_head] = self.k_linear.weight.view(self.heads, self.d_head, self.d_model)
             q_bias[:, :self.d_head] = self.q_linear.bias.view(self.heads, self.d_head)
             k_bias[:, :self.d_head] = self.k_linear.bias.view(self.heads, self.d_head)
 
@@ -130,15 +134,13 @@ class ScaledDotProductAttention(GrowingModule):
 
             scale = torch.sqrt(torch.tensor(1 + d_new / self.d_head))
 
-            q_weight /= scale
-            #k_weight /= scale
-            q_bias /= scale
-            #k_bias /= scale
+            q_weight *= scale
+            q_bias *= scale
 
             self.d_head = self.d_head + d_new
 
-            self.q_linear.weight = torch.nn.Parameter(q_weight.reshape(self.heads*self.d_head, -1))
-            self.k_linear.weight = torch.nn.Parameter(k_weight.reshape(self.heads*self.d_head, -1))
+            self.q_linear.weight = torch.nn.Parameter(q_weight.reshape(self.heads*self.d_head, self.d_model))
+            self.k_linear.weight = torch.nn.Parameter(k_weight.reshape(self.heads*self.d_head, self.d_model))
             self.q_linear.bias = torch.nn.Parameter(q_bias.reshape(self.heads*self.d_head))
             self.k_linear.bias = torch.nn.Parameter(k_bias.reshape(self.heads*self.d_head))
 

tests/test_degrow.py
@@ -3,9 +3,6 @@ function. """
 
 import pytest
 
-from itertools import product
-from copy import deepcopy
-
 import torch
 
 from growing import MLP, ScaledDotProductAttention
@@ -41,20 +38,20 @@ def test_degrow(grow_params, model_spec):
 
     model_type, model_args = model_spec
 
-    model_a = model_type(*model_args)
+    model = model_type(*model_args)
 
-    sizes = model_a.grow(**grow_params)
+    x = torch.rand(64, 10, model.in_features)
 
-    model_b = deepcopy(model_a)
+    sizes = model.grow(**grow_params)
+
+    y_a = model(x)
 
     # degrow keeping all neurons
-    model_b.degrow(select_all(sizes))
+    model.degrow(select_all(sizes))
 
-    x = torch.rand(64, 10, model_a.in_features)
+    y_b = model(x)
 
-    diff = torch.abs(model_a(x) - model_b(x))
-
-    print(diff.max())
+    diff = torch.abs(y_a - y_b)
 
     assert torch.all(diff < eps)
 
@@ -72,17 +69,18 @@ def test_degrow_completely(grow_params, model_spec):
 
     model_type, model_args = model_spec
 
-    model_a = model_type(*model_args)
-    model_b = deepcopy(model_a)
+    model = model_type(*model_args)
 
-    model_b.grow(**grow_params)
+    x = torch.rand(64, 10, model.in_features)
+
+    y_a = model(x)
+
+    model.grow(**grow_params)
     # degrow deleting all recently grown neurons
-    model_b.degrow(torch.arange(0))
+    model.degrow(torch.arange(0))
 
-    x = torch.rand(64, 10, model_a.in_features)
+    y_b = model(x)
 
-    diff = torch.abs(model_a(x) - model_b(x))
-
-    print(diff.max())
+    diff = torch.abs(y_a - y_b)
 
     assert torch.all(diff < eps)

tests/test_grow.py
@@ -3,9 +3,6 @@ function. """
 
 import pytest
 
-from itertools import product
-from copy import deepcopy
-
 import torch
 
 from growing import MLP, ScaledDotProductAttention
@@ -32,14 +29,17 @@ def test_growth(grow_params, model_spec):
 
     model_type, model_args = model_spec
 
-    model_a = model_type(*model_args)
-    model_b = deepcopy(model_a)
+    model = model_type(*model_args)
 
-    model_b.grow(**grow_params)
+    x = torch.rand(64, 10, model.in_features)
 
-    x = torch.rand(64, 10, model_a.in_features)
+    y_a = model(x)
 
-    diff = torch.abs(model_a(x) - model_b(x))
+    model.grow(**grow_params)
+
+    y_b = model(x)
+
+    diff = torch.abs(y_a - y_b)
 
     # the change should be minor
     assert torch.all(diff < eps)