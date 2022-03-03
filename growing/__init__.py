6b1ab5b54bacdd99ad50c7cb727b005c4705cfef
Max Ploner <plonerma@maxploner.de>
2022-03-03 12:19:13 +0100

    Renamed dot product

--- growing/__init__.py
+++ growing/__init__.py
@@ -1,4 +1,4 @@
 from .mlp import MLP
-from .scaled_dot_product_attention import ScaledDotProductAttention
+from .scaledDotProductAttention import ScaledDotProductAttention
 from .transformer import GrowingTransformer
 from .multiheadAttention import MultiheadAttention
