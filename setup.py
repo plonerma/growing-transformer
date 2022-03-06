459bd788a9cfc0a8fea5e5791fac5aebd2114734
Max Ploner <plonerma@maxploner.de>
2022-03-06 21:11:30 +0100

    Added mypy to dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[
         "torch",
+        "transformers",
         "pytest",
-        "transformers"
+        "mypy",
     ],
 )
