36bfe4aaa975129ec2dee55e5b090b31595cb18d
Max Ploner <plonerma@maxploner.de>
2022-03-07 18:18:10 +0100

    Added github action for testing

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
 
 setup(
-    name='DynamicallyGrowingTransformer',
-    version='0.1.0',
-    author='Max Ploner',
-    author_email='growing_transformer@maxploner.de',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},  # Optional
+    name="DynamicallyGrowingTransformer",
+    version="0.1.0",
+    author="Max Ploner",
+    author_email="growing_transformer@maxploner.de",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},  # Optional
     package_data={"src/growing_transformer": ["py.typed"]},
-    description='Dynamically Growing Transformer Using Firefly Neural Architecture Descent',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "torch",
-        "transformers",
-        "pytest",
-        "mypy",
-        "matplotlib"
-    ],
+    description="Dynamically Growing Transformer Using Firefly Neural Architecture Descent",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
 )
