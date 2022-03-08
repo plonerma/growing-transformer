c2e1449782b3bf797d2ddf7b2485bb3dfbb0a318
Max Ploner <plonerma@maxploner.de>
2022-03-08 12:00:28 +0100

    Added state_dict testing (function should be able to be transferred between two model); added mistakenly removed test_minor_growth_change in test base.

--- tests/base.py
+++ tests/base.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod
-from typing import Tuple
+from typing import List, Tuple
 
 import pytest
 import torch
 
-from growing_transformer import GrowingModule
+from growing_transformer import Growing, GrowingModule
 
 
 class GrowingTest:
@@ -14,7 +14,7 @@ class GrowingTest:
 
     growth_params = [
         dict(num_novel=4, eps_novel=1, step_size=1e-5),
-        dict(num_novel=4, eps_novel=1e-4, step_size=1),
+        dict(num_novel=4, eps_novel=1e-5, step_size=1),
     ]
 
     degrowth_params = [
@@ -32,6 +32,53 @@ class GrowingTest:
     def random_batch(self):
         return torch.rand(self.batches, self.length, self.embed_dim)
 
+    def test_state_loading(self):
+        model_a = self.new_model({})
+        model_b = self.new_model({})
+
+        state = model_a.state_dict()
+        model_b.load_state_dict(state)
+
+        model_a.eval()
+        model_b.eval()
+
+        x = self.random_batch()
+        y_a = model_a(x)
+        y_b = model_b(x)
+
+        diff = torch.abs(y_a - y_b)
+
+        assert torch.all(diff < 1e-10)
+
+    @pytest.mark.parametrize("config", growth_params)
+    def test_growth_is_minor(self, config):
+        """With these growth parameters, the function of the network should be
+        changed only a litte bit.
+        """
+
+        torch.manual_seed(0)
+
+        model = self.new_model(config)
+
+        model.eval()
+
+        x = self.random_batch()
+
+        y_a = model(x)
+
+        if isinstance(model, GrowingModule):
+            model.grow()
+        else:
+            for m in model.growing_modules():
+                m.grow()
+
+        y_b = model(x)
+
+        diff = torch.abs(y_a - y_b)
+
+        # the change should be minor
+        assert torch.all(diff < 1e-2)
+
     @pytest.mark.parametrize("config", growth_params)
     def test_growth_exists(self, config):
         """With these growth parameters, the function of the network should be
@@ -41,18 +88,26 @@ class GrowingTest:
 
         model = self.new_model(config)
 
+        model.eval()
+
         x = self.random_batch()
 
         y_a = model(x)
 
-        model.grow()
+        if isinstance(model, GrowingModule):
+            # grow only the module itself
+            model.grow()
+        else:
+            # test for composite growth
+            for m in model.growing_modules():
+                m.grow()
 
         y_b = model(x)
 
         diff = torch.abs(y_a - y_b)
 
         # but should be there
-        assert torch.any(diff > 1e-10)
+        assert torch.any(diff > 1e-12)
 
     @pytest.mark.parametrize("config", degrowth_params)
     def test_degrow_to_grown(self, config):
@@ -64,14 +119,23 @@ class GrowingTest:
 
         model = self.new_model(config)
 
+        model.eval()
+
         x = self.random_batch()
 
-        size = model.grow()
+        modules: List[Growing]
+        if isinstance(model, GrowingModule):
+            modules = [model]
+        else:
+            modules = list(model.growing_modules())
+
+        sizes: List[torch.Size] = [m.grow() for m in modules]
 
         y_a = model(x)
 
         # degrow keeping all neurons
-        model.degrow(torch.arange(size.numel()))
+        for m, size in zip(modules, sizes):
+            m.degrow(torch.arange(size.numel()))
 
         y_b = model(x)
 
@@ -88,14 +152,23 @@ class GrowingTest:
 
         model = self.new_model(config)
 
+        model.eval()
+
         x = self.random_batch()
 
         y_a = model(x)
 
-        model.grow()
+        modules: List[Growing]
+        if isinstance(model, GrowingModule):
+            modules = [model]
+        else:
+            modules = list(model.growing_modules())
+
+        sizes: List[torch.Size] = [m.grow() for m in modules]
 
         # degrow deleting all recently grown neurons
-        model.degrow(torch.arange(0))
+        for m, size in zip(modules, sizes):
+            m.degrow(torch.arange(0))
 
         y_b = model(x)
 
@@ -122,11 +195,17 @@ class SplittingTest(GrowingTest):
 
         model = self.new_model(config)
 
+        model.eval()
+
         x = self.random_batch()
 
         y_a = model(x)
 
-        model.grow()
+        if isinstance(model, GrowingModule):
+            model.grow()
+        else:
+            for m in model.growing_modules():
+                m.grow()
 
         y_b = model(x)
 
