3188d7a278cd832519b9fd62d2eed5ad631f6228
Max Ploner <plonerma@maxploner.de>
2022-03-18 15:07:34 +0100

    Changed pretrain scripts

--- examples/pretrain_static.py
+++ examples/pretrain_static.py
@@ -6,16 +6,20 @@ import torch
 from datasets import DatasetDict
 from transformers import BertTokenizer
 
+import growing_transformer
+
 from growing_transformer import (
     BaseTrainer,
     GrowingConfig,
     GrowingMLMTransformer,
-    GrowthSchedule,
+    GrowthSchedule
 )
 from growing_transformer.data import MLMSegmenetDataset
 from growing_transformer.trainer.util import add_file_handler
 
-base_path = Path("pretrained_static")
+growing_transformer.device = 'cuda:2'
+
+base_path = Path("results/pretrained_static")
 base_path.mkdir(exist_ok=True, parents=True)
 
 log = logging.getLogger("growing_transformer")
@@ -27,7 +31,7 @@ corpus = datasets.load_dataset("wikitext", "wikitext-2-raw-v1")
 
 assert isinstance(corpus, DatasetDict)
 
-tokenizer = BertTokenizer.from_pretrained("./models/tokenizer/bert-base-cased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 
 train_data = MLMSegmenetDataset(corpus["train"], tokenizer).downsampled(0.1)
 test_data = MLMSegmenetDataset(corpus["test"], tokenizer)
