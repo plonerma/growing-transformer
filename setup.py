ba3b4dd0fa3067658c4822f50d027f3c7079f4bc
Max Ploner <plonerma@maxploner.de>
2022-04-13 20:26:19 +0200

    Added manifest

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="growing_transformer@maxploner.de",
     packages=find_packages(where="src"),
     package_dir={"": "src"},  # Optional
-    package_data={"src/growing_transformer": ["py.typed"]},
+    include_package_data=True,
     description="Dynamically Growing Transformer Using Firefly Neural Architecture Descent",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
