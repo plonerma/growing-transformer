Removed gradient clipping (not usually used with grad scaling) & fixed some minor issues

examples/finetune.py
@@ -78,7 +78,7 @@ def main(args):
     )
 
     if args.save_model is not None:
-        torch.save(model.state_dict(), parser.save_model)
+        torch.save(model.state_dict(), args.save_model)
 
 
 if __name__ == "__main__":

examples/pretrain.py
@@ -1,14 +1,15 @@
 import logging
 import random
-from typing import Any, Dict, Union
 from pathlib import Path
+from typing import Any, Dict, Union
+
 import datasets
 import hydra
-from hydra.utils import get_original_cwd
 import torch
 from config import Configuration
 from datasets import DatasetDict
 from hydra.core.config_store import ConfigStore
+from hydra.utils import get_original_cwd
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from transformers import BertForMaskedLM, BertTokenizer, DataCollatorForLanguageModeling
@@ -179,7 +180,6 @@ def main(cfg: Configuration):
         eps=cfg.training.eps,
         max_lr=cfg.training.learning_rate,
         weight_decay=cfg.training.weight_decay,
-        gradient_clipping=cfg.training.gradient_clipping,
     )
 
     def custom_eval(self, global_step=None):

src/growing_transformer/trainer/base.py
@@ -61,6 +61,9 @@ class BaseTrainer:
     def get_lr_scheduler(
         self, *, optimizer, type: str, warmup_steps: int = None, total_steps: int = None, last_step: int = None
     ):
+        if last_step is None:
+            last_step = -1
+
         if type == "constant":
             return LambdaLR(optimizer, lambda step: 1, last_step)
         elif type == "linear":
@@ -71,9 +74,6 @@ class BaseTrainer:
                 if step < warmup_steps:
                     return float(step) / float(max(1, warmup_steps))
                 return max(0.0, float(total_steps - step) / float(max(1, total_steps - warmup_steps)))
-
-            if last_step is None:
-                last_step = -1
 
             return LambdaLR(optimizer, lr_lambda, last_step)
         else:
@@ -90,7 +90,6 @@ class BaseTrainer:
         betas: Tuple[float, float] = (0.9, 0.98),
         eps: float = 1e-06,
         weight_decay: float = 0.01,
-        gradient_clipping: float = None,
         gca_batches: int = 16,  # gradient accumulation batches
         num_epochs: int = 5,
         batch_size: int = 32,
@@ -219,9 +218,6 @@ class BaseTrainer:
 
                     if (batch_index % gca_batches == 0) or (batch_index == len(batch_loader)):
                         global_step += 1
-
-                        # if gradient_clipping is not None:
-                        #    torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=gradient_clipping)
 
                         scaler.step(optimizer)
                         scaler.update()

src/growing_transformer/trainer/growing.py
@@ -355,10 +355,6 @@ class GrowingTrainer(BaseTrainer):
         except KeyboardInterrupt:
             log_line(log)
 
-            if propagate_interrupt:
-                log.warning("Exiting from training early.")
-                raise KeyboardInterrupt
-
             if test_data is not None:
                 log.info("Evaluating after early termination:")
                 eval_results = self.evaluate(test_data)
@@ -395,11 +391,11 @@ class GrowingTrainer(BaseTrainer):
         with self.some_grad_only(*self.model.step_size_params()):
             self.model.zero_grad(set_to_none=True)
 
-            #scaler = torch.cuda.amp.GradScaler()
+            # scaler = torch.cuda.amp.GradScaler()
 
             for batch in self.get_batch_loader(train_data, batch_size=batch_size):
-                #with torch.cuda.amp.autocast():
+                # with torch.cuda.amp.autocast():
                 loss = self.forward_loss(batch)
-                #scaler.scale(loss).backward()
+                # scaler.scale(loss).backward()
                 loss.backward()
-                #scaler.update()
+                # scaler.update()